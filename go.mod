e80b5224d599e19646c74fb0826141c18eff91e9
Animus PEXUS <animuspexus@protonmail.com>
2021-03-29 00:17:21 +0300

    ic

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/AnimusPEXUS/gojsfirebase
 go 1.16
 
 require (
-	github.com/AnimusPEXUS/gojstools v0.0.0-20210327193209-4c20672e96a8
-	github.com/AnimusPEXUS/gojswebapi v0.0.0-20210327193144-5b90f8f50350
+	github.com/AnimusPEXUS/gojstools v0.0.0-20210328204158-da53b8f868c3
+	github.com/AnimusPEXUS/gojswebapi v0.0.0-20210328203346-9fb86e8f8f84
 )
---
+++ widgets/FirebasePhoneLoginPhone00/FirebasePhoneLoginPhone00.go
@@ -0,0 +1,98 @@
+package FirebasePhoneLoginPhone00
+
+import (
+	// "syscall/js"
+
+	"github.com/AnimusPEXUS/gojsfirebase"
+	"github.com/AnimusPEXUS/gojstools/elementtreeconstructor"
+)
+
+type FirebasePhoneLoginPhone00Options struct {
+	etc *elementtreeconstructor.ElementTreeConstructor
+
+	LoadFirebaseAuthonomously bool
+	GetAuthCB                 func() (*gojsfirebase.Auth, error)
+}
+
+type FirebasePhoneLoginPhone00 struct {
+	options *FirebasePhoneLoginPhone00Options
+
+	Element *elementtreeconstructor.ElementMutator
+}
+
+// type LoginPasswordForm00 struct {
+// 	etc *elementtreeconstructor.ElementTreeConstructor
+
+// 	put_submit_button bool
+
+// 	onedit         func()
+// 	onloginedit    func()
+// 	onpasswordedit func()
+// 	onsubmitclick  func()
+
+// 	login_input    *elementtreeconstructor.ElementMutator
+// 	password_input *elementtreeconstructor.ElementMutator
+
+// 	Element *elementtreeconstructor.ElementMutator
+// }
+
+func NewFirebasePhoneLoginPhone00(options *FirebasePhoneLoginPhone00Options) (
+	*FirebasePhoneLoginPhone00,
+	error,
+) {
+
+	self := &FirebasePhoneLoginPhone00{
+		options: options,
+	}
+
+	etc := self.options.etc
+
+	self.Element = etc.CreateElement("table")
+
+	self.Element.AppendChildren(
+		etc.CreateElement("tr").
+			AppendChildren(
+				etc.CreateElement("td").
+					AppendChildren(
+						etc.CreateTextNode("Phone:"),
+					),
+				etc.CreateElement("td").
+					AppendChildren(
+						etc.CreateElement("input").
+							SetAttribute("type", "text"),
+					),
+				etc.CreateElement("td").
+					AppendChildren(
+						etc.CreateElement("button").
+							AppendChildren(
+								etc.CreateTextNode("Begin Phone Authentication Process"),
+							),
+					),
+			),
+		etc.CreateElement("tr").
+			AppendChildren(
+				etc.CreateElement("td"),
+			),
+		etc.CreateElement("tr").
+			AppendChildren(
+				etc.CreateElement("td").
+					AppendChildren(
+						etc.CreateTextNode("Confirnation Code:"),
+					),
+				etc.CreateElement("td").
+					AppendChildren(
+						etc.CreateElement("input").
+							SetAttribute("type", "text"),
+					),
+				etc.CreateElement("td").
+					AppendChildren(
+						etc.CreateElement("button").
+							AppendChildren(
+								etc.CreateTextNode("Begin Phone Authentication Process"),
+							),
+					),
+			),
+	)
+
+	return self, nil
+}
